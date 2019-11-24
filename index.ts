eb3fb47f36da3a83f8e15d4471c2478f7cb29100
Vertepnyi Dmytro <winerr22@gmail.com>
2019-11-24 15:20:44 +0200

    Fixed index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export {TextModule} from './src/feature/text.module'
-export {EditFontSizeDirective} from './src/feature/increase-font-size.component'
+export { TextModule } from './src/feature/text.module'
+export { EditFontSizeDirective } from './src/feature/edit-font-size.directive'
